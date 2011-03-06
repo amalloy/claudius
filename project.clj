b5288759f54fbf4b522579c12654abd008e93d8b
Alan Malloy <alan@malloys.org>
2011-03-05 23:33:34 -0800

    Sketch out a first working version

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject claudius "0.0.1-SNAPSHOT"
+  :description "TODO: add summary of your project"
+  :dependencies [[clojure "1.2.0"]
+                 [clojure-contrib "1.2.0"]
+                 [org.clojars.amalloy/utils "[0.1.0,)"]
+                 [fnparse "[2.2.0,)"]])
